a32959cba7d16a62d6344485e382bffab8a96fc3
himanshukr9006 <studyhimanshu9006@gmail.com>
2026-05-24 15:48:18 +0530

    added more fruits names

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/api')
 def api():
     
-    data = ["Apple", "Banana", "Mango"]
+    data = ["Apple", "Banana", "Mango", "papaya", "palm", "strawberry"]
 
     return jsonify(data)
 
